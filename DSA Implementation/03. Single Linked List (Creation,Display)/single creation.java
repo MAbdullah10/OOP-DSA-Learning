29578ace48b906a5756a3b7d50827bbac0726897
MAbdullah10 <120081761+MAbdullah10@users.noreply.github.com>
2024-06-21 03:23:20 +0500

    Add files via upload

---
+++ DSA Implementation/03. Single Linked List (Creation,Display)/single creation.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+class single_creation
+{
+    static class Node
+    {
+            int data;
+            Node next;
+            Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+    }
+    Node head = null;
+    public void creation() {
+        int data,a;
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.println("Enter data: ");
+            data = s.nextInt();
+            Node n = new Node(data);
+            if (head == null) {
+                head = n;
+            } else {
+                n.next = head;
+                head = n;
+            }
+            System.out.println("To enter again press 1: ");
+            a = s.nextInt();
+        }
+        while (a == 1) ;
+    }
+    public void display()
+    {
+        Node temp=head;
+        if(head==null)
+        {
+            System.out.println("List is empty");
+        }
+        else
+            while(temp!=null)
+            {
+                System.out.println(temp.data);
+                temp =temp.next;
+            }
+    }
+
+    public static void main(String[] arg)
+    {
+        single_creation l =new single_creation();
+        l.creation();
+        l.display();
+    }
+}
